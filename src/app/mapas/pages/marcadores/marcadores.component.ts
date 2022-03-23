620d31ef00414d72e8eaef0504980728e05424cb
Carlos Mestas <carlos93esp@gmail.com>
2022-03-23 23:19:12 +0100

    Marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MarcadorColor{
+  color: string;
+  marker: mapboxgl.Marker;
+}
+
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styleUrls: ['./marcadores.component.css']
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-3.8132679314591855,43.45644490830871];
+  marcadores: MarcadorColor[] = [];
+
 
   constructor() { }
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
 
-  ngOnInit(): void {
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'Hola Mundo';
+
+
+    // const marker = new mapboxgl.Marker()
+    // .setLngLat(this.center).addTo(this.mapa)
+
+  }
+
+  irMarcador(marcador: mapboxgl.Marker){
+    this.mapa.flyTo({
+      center: marcador.getLngLat()
+    });
+    
+    
+    
+    
+  }
+
+  agregarMarcador(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color
+    })
+    .setLngLat(this.center)
+    .addTo(this.mapa);
+
+    this.marcadores.push({
+      color,
+      marker: nuevoMarcador
+    });
+    this.guardarMarcadoresLocalStorage(nuevoMarcador);
+  }
+
+  guardarMarcadoresLocalStorage(nuevoMarcador: mapboxgl.Marker){
+    localStorage.setItem(JSON.stringify(nuevoMarcador.getLngLat()), JSON.stringify(nuevoMarcador))
+
+
+  }
+
+  leerMarcadoresLocalStorage(){
+
   }
 
 }
--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import * as mapboxgl  from 'mapbox-gl';
 
 @Component({
@@ -6,23 +6,42 @@ import * as mapboxgl  from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.css']
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-3.8132679314591855,43.45644490830871];
 
   constructor() { }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', ()=>{});
+    this.mapa.off('zoomend', ()=>{});
+    this.mapa.off('move', ()=>{});
+  }
 
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-3.8132679314591855,43.45644490830871],
+      center: this.center,
       zoom: this.zoomLevel
     });
 
     this.mapa.on('zoom', (ev) =>{
-      this.mapa.getZoom()
+      this.zoomLevel = this.mapa.getZoom();
+    })
+
+    this.mapa.on('zoomend', (ev) =>{
+      if(this.mapa.getZoom() > 18){
+        this.mapa.zoomTo(18);
+      }
+    })
+
+    this.mapa.on('move', (event)=>{
+      const target = event.target;
+      const {lng, lat} = target.getCenter();
+      this.center = [lng, lat];
     })
   }
 
@@ -34,6 +53,10 @@ export class ZoomRangeComponent implements AfterViewInit {
   zoomIn(){
     this.mapa.zoomIn();
     this.zoomLevel = this.mapa.getZoom();
+  }
+
+  zoomCambio(valor: string){
+    this.mapa.zoomTo(Number(valor));
   }
 
 }
